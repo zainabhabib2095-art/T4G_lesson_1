0f6c3055ef10f5e14949a0fa2be69eb125f6e3d5
zainabhabib2095-art <zainabhabib2095@gmail.com>
2026-04-01 08:37:53 -0400

    adding all changes

---
+++ Lesson1/src/script.js
@@ -0,0 +1,15 @@
+alert("Hello, World!");
+let x=2;
+// alert(x);
+// let name = "Zainab";
+// alert(name);
+// const pi = 3.14;
+// alert(pi);
+// pi = 3.14159; // This will cause an error because pi is a constant
+//prompt("What is your name?");
+let userName = prompt ("What is your name?");
+alert("Hello, " + userName + "!" + " You are welcome to learn JavaScript with me!");
+let sentence = "I" + " am " + "learning " + "JavaScript." + " " + userName;
+alert(sentence);
+alert(`Hello, ${userName}! You are learning JavaScript!`);
+alert(`You are ${age} years old!`);
